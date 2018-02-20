f6d00140c598bf4032e446f4c325f645293b7d07
Thibault <thibault.bourgeois@etu.u-pec.fr>
2018-02-20 16:39:21 +0100

    add Project

---
+++ src/main/java/fr/project/MyMain.java
@@ -0,0 +1,17 @@
+package fr.project;
+
+/**
+ * @author BOURGEOIS Thibault
+ * Date     20/02/2018
+ * Time     16:37
+ */
+public class MyMain {
+
+    public static void main(String[] args) {
+        for (int i=0; i<5; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+}
