14b6fa9e66fe03fdd2db5c39de338c445da3d20b
Thibault <thibault.bourgeois@etu.u-pec.fr>
2018-03-02 02:58:59 +0100

    remove utf8

--- restservice/src/main/resources/import.sql
+++ restservice/src/main/resources/import.sql
@@ -5,7 +5,7 @@ INSERT INTO sample (my_string) VALUES ('test4');
 INSERT INTO sample (my_string) VALUES ('test5');
 
 INSERT INTO user (date_of_birth, first_name, last_name, mail, password, pseudo) VALUES ('27/02/2012', 'Thibault', 'BOURGEOIS', 'titi@titi.fr', 'mypass', 'ncna');
-INSERT INTO user (date_of_birth, first_name, last_name, mail, password, pseudo) VALUES ('06/07/1993', 'Ruben', 'EDERY', 'rub@rub.fr', 'mypass', 'lejuif');
+INSERT INTO user (date_of_birth, first_name, last_name, mail, password, pseudo) VALUES ('06/07/1993', 'Ruben', 'EDERY', 'rub@rub.fr', 'mypass', 'tété');
 
 INSERT INTO workplace (wording) VALUES ('Chef');
 INSERT INTO workplace (wording) VALUES ('Ouvrier');
